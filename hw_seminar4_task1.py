044bacbd88070125d699dd0bd3ffeb6ab05c01a0
“[Aleksandr] <[fraoo@icloud.coml]>
2023-05-30 21:32:43 +0300

    home_work4

---
+++ hw_seminar4_task1.py
@@ -0,0 +1,25 @@
+# Задача 1. Дано натуральное число N. 
+# Напишите метод, который вернёт список простых множителей
+# числа N и количество этих множителей.
+# 60 -> 2, 2, 3, 5
+# 1. Разложим число на множители
+def getListMult(numN):
+    list = []
+    listMult = [list.append(i) for i in range(1, numN+1) if numN % i == 0]
+    return list
+
+
+# 2. Составим список простых множителей заданного числа
+def primeNum(numN):
+    primeList = []
+    for i in range(2, numN):
+        while numN % i == 0:
+            numN /= i
+            primeList.append(i)
+    return primeList
+
+numN = int(input("Введите натуральное число: "))
+list1 = getListMult(numN)
+print(f'Множители числа {numN} : {list1}')
+list2 = primeNum(numN)
+print(f'Простые множители числа {numN} :{list2}')
