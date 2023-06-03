b8338ded29894cfee2bb3068d57424df6b17a73c
“[Aleksandr] <[fraoo@icloud.coml]>
2023-06-03 10:59:09 +0300

    home_work9+

--- hw_seminar9_task1.py
+++ hw_seminar9_task1.py
@@ -8,7 +8,4 @@ print(numbers)
 uniq_list, uniq_counts = np.unique(numbers, return_counts=True)
 print(f'уникальные элементы {uniq_list}')
 print(f'их количество       {uniq_counts}')
-index_max = np.argmax(uniq_counts)
-print(index_max)
-print(f'Элемент {uniq_list[index_max]} встречает {uniq_counts[index_max]}')
 
